e21b93ac49b74b6bcd1d2160897a718171d7f547
Hilman <hilmansaukani74@gmail.com>
2026-05-18 16:02:10 +0700

    bikin middleware

--- resources/views/backend/v_beranda/index.blade.php
+++ resources/views/backend/v_beranda/index.blade.php
@@ -1,5 +1,17 @@
-<h3> {{$judul}} </h3> 
-<p> 
-    Selamat Datang, <b>Nama_User</b> pada aplikasi Toko Online dengan hak akses yang anda 
-miliki sebagai <b>Role_User</b> ini adalah halaman utama dari aplikasi ini. 
-</p> 
+@extends('backend.v_layouts.app')
+@section('content')
+    <!-- contentAwal -->
+    <h3> {{$judul}} </h3>
+    <p>
+        ses yang anda miliki sebagai
+        <b>
+            @if (Auth::user()->role == 1)
+                Super Admin
+            @elseif(Auth::user()->role == 0)
+                Admin
+            @endif
+        </b>
+        ini adalah halaman utama dari aplikasi ini.
+    </p>
+    <!-- contentAkhir -->
+@endsection

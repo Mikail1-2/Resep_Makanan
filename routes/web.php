61e1705666a38deb855eb514ca3423012e9ceb16
unknown <ri0sabl3ng@gmail.com>
2026-05-19 22:48:54 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
-use Illuminate\Support\Facades\Route;use App\Http\Controllers\BerandaController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-return view('welcome');
-});
+Route::get('/', [BerandaController::class, 'berandaBackend'])->name('dashboard');
+
 Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
 Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
 Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
 Route::get('backend/register', [RegisterController::class, 'index'])->name('register');
 Route::post('backend/register', [RegisterController::class, 'store'])->name('register.store');
-Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda')->middleware('auth');
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda');
+Route::get('backend/profile', [ProfileController::class, 'index'])->middleware('auth')->name('backend.profile');
